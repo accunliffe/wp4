c807099411d891d04432b7ade2a41db49c03389c
Andrew Cunliffe <rhinocapitalmgmt@gmail.com>
2014-06-24 19:18:48 -0700

    switched to template and directive

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -79,15 +79,9 @@ var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
 };
 
 var ButtonsCtrl = function ($scope) {
-
-  $scope.singleModel = 1;
-
-  $scope.radioModel = 'Middle';
-
   $scope.checkModel = {
     left: false,
     middle: true,
     right: false
   };
 };
-
